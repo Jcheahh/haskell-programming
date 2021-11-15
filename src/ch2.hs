7715513e44ebd749941ddba715923cc287e65058
jack <jackcheah2@gmail.com>
2021-11-15 20:45:34 +0800

    Finished chapter2

---
+++ src/ch2.hs
@@ -0,0 +1,20 @@
+triple :: Integer -> Integer
+triple x = x * 3
+
+times3pointonefour :: Float -> Float
+times3pointonefour x = x * pi
+
+thirdLetter :: String -> Char
+thirdLetter x = x !! 2
+
+z = 7
+
+y = z + 8
+
+x = y ^ 2
+
+waxOn = x1 * 5
+  where
+    x1 = x
+
+waxOff = triple
