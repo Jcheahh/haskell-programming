d5d99aafe751b3b120f77c5b2bcf43317edb9f3e
jack <jackcheah2@gmail.com>
2021-11-23 17:25:21 +0800

    Finished morse code for chapter 14

--- haskellbook/morse/app/Main.hs
+++ haskellbook/morse/app/Main.hs
@@ -1,6 +1,63 @@
 module Main where
 
-import Lib
+import Control.Monad (forever, when)
+import Data.List (intercalate)
+import Data.Traversable (traverse)
+import Morse (morseToChar, stringToMorse)
+import System.Environment (getArgs)
+import System.Exit
+  ( exitFailure,
+    exitSuccess,
+  )
+import System.IO (hGetLine, hIsEOF, stdin)
 
 main :: IO ()
-main = someFunc
+main = do
+  mode <- getArgs
+  case mode of
+    [arg] ->
+      case arg of
+        "from" -> convertFromMorse
+        "to" -> convertToMorse
+        _ -> argError
+        _ -> argError
+  where
+    argError = do
+      putStrLn
+        "Please specify the\
+        \ first argument\
+        \ as being 'from' or\
+        \ 'to' morse,\
+        \ such as: morse to"
+      exitFailure
+
+convertToMorse :: IO ()
+convertToMorse = forever $ do
+  weAreDone <- hIsEOF stdin
+  when weAreDone exitSuccess
+  line <- hGetLine stdin
+  convertLine line
+  where
+    convertLine line = do
+      let morse = stringToMorse line
+      case morse of
+        Nothing -> do
+          putStrLn $ "ERROR: " ++ line
+          exitFailure
+        Just str -> putStrLn (intercalate " " str)
+
+convertFromMorse :: IO ()
+convertFromMorse = forever $ do
+  weAreDone <- hIsEOF stdin
+  when weAreDone exitSuccess
+  line <- hGetLine stdin
+  convertLine line
+  where
+    convertLine line = do
+      let decode :: Maybe String
+          decode = traverse morseToChar (words line)
+      case decode of
+        Nothing -> do
+          putStrLn $ "ERROR: " ++ line
+          exitFailure
+        Just s -> putStrLn s
