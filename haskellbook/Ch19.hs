ea25546b5aee12eb86ee84fe61888fafab5a486d
jack <jackcheah2@gmail.com>
2021-12-03 11:06:31 +0800

    Finished chapter 19

--- haskellbook/Ch19.hs
+++ haskellbook/Ch19.hs
@@ -1,1 +1,28 @@
 module Ch19 where
+
+import Control.Applicative (liftA2)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Time.Clock
+import qualified Data.UUID as UUID
+import qualified Data.UUID.V4 as UUIDv4
+
+offsetCurrentTime ::
+  NominalDiffTime ->
+  IO UTCTime
+offsetCurrentTime offset =
+  fmap (addUTCTime (offset * 24 * 3600)) $
+    getCurrentTime
+
+textUuid :: IO Text
+textUuid =
+  fmap
+    (T.pack . UUID.toString)
+    UUIDv4.nextRandom
+
+(<||>) ::
+  (a -> Bool) ->
+  (a -> Bool) ->
+  a ->
+  Bool
+(<||>) = liftA2 (||)
